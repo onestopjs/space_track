36f2fcfdb58a64dbde42e96ae4de1a8aca34dbc5
Martin Koparanov <m.n.koparanov@gmail.com>
2024-05-04 16:57:21 +0300

    Refactor deserializers

--- src/space_track/utils.rs
+++ src/space_track/utils.rs
@@ -1,28 +1,45 @@
+use std::str::FromStr;
+
 use serde::{de, Deserialize, Deserializer};
 use serde_json::Value;
 
-pub fn deserialize_optional_string_to_u64<'de, D: Deserializer<'de>>(
+fn deserialize_string<'de, D: Deserializer<'de>, T: FromStr>(deserializer: D) -> Result<T, D::Error>
+where
+    <T as FromStr>::Err: std::fmt::Display,
+{
+    Ok(match Value::deserialize(deserializer)? {
+        Value::String(s) => s.parse().map_err(de::Error::custom)?,
+        _ => return Err(de::Error::custom("wrong type")),
+    })
+}
+
+fn deserialize_optional_string<'de, D: Deserializer<'de>, T: FromStr>(
     deserializer: D,
-) -> Result<Option<u64>, D::Error> {
+) -> Result<Option<T>, D::Error>
+where
+    <T as FromStr>::Err: std::fmt::Display,
+{
     Ok(match Value::deserialize(deserializer)? {
         Value::String(s) => Some(s.parse().map_err(de::Error::custom)?),
-        Value::Number(num) => Some(
-            num.as_u64()
-                .ok_or_else(|| de::Error::custom("Invalid number"))?,
-        ),
         Value::Null => None,
         _ => return Err(de::Error::custom("wrong type")),
     })
 }
 
+pub fn deserialize_optional_string_to_u64<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<Option<u64>, D::Error> {
+    deserialize_optional_string(deserializer)
+}
+
 pub fn deserialize_string_to_u64<'de, D: Deserializer<'de>>(
     deserializer: D,
 ) -> Result<u64, D::Error> {
-    Ok(match Value::deserialize(deserializer)? {
-        Value::String(s) => s.parse().map_err(de::Error::custom)?,
-        Value::Number(num) => num
-            .as_u64()
-            .ok_or_else(|| de::Error::custom("Invalid number"))?,
-        _ => return Err(de::Error::custom("wrong type")),
-    })
+    deserialize_string(deserializer)
+}
+
+pub fn deserialize_string_to_u8<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<u8, D::Error> {
+    deserialize_string(deserializer)
 }
